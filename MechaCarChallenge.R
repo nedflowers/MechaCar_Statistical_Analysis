fd8ac99d6962616dfd1b38f1b262a1d316ac58ca
Pau Rodriguez <pau@apothca.com>
2022-04-10 19:04:11 -0700

    dev 3 and imgs

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,9 @@ summary_coil_data
 
 lot_summary <- suspension_coil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create lot summary table
 lot_summary
+
+t.test(suspension_coil_data$PSI, mu=1500) # run T-test on coils
+
+t.test(subset(suspension_coil_data,Manufacturing_Lot=="Lot1")$PSI, mu = 1500) # Lot1 t-test
+t.test(subset(suspension_coil_data,Manufacturing_Lot=="Lot2")$PSI, mu = 1500) # Lot2 t-test
+t.test(subset(suspension_coil_data,Manufacturing_Lot=="Lot3")$PSI, mu = 1500) # Lot3 t-test
